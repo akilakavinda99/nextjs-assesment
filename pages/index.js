fa8e3996b113b00dc3f85995d565c571bd9c33e2
akilakavinda99 <akilakavinda909@gmail.com>
2023-01-09 22:58:40 +0530

    Navbar component implementation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <NavBar />
     </>
   );
 }
