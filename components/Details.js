f6929cf044399738baf1fa757ac69c0e1c8ad51a
akilakavinda99 <akilakavinda909@gmail.com>
2023-01-10 11:04:32 +0530

    Details section implementation

---
+++ components/Details.js
@@ -0,0 +1,17 @@
+import { Line, Wrapper } from "../styles/styledComponents/DetailsStyles";
+
+export default function Details() {
+  return (
+    <Wrapper>
+      <Line />
+      <h3>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries,
+      </h3>
+      <Line />
+    </Wrapper>
+  );
+}
---
+++ styles/styledComponents/DetailsStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 30px;
+  margin-bottom: 35px;
+`;
+export const Line = styled.hr`
+  border-top: 3px solid #bbb;
+`;
